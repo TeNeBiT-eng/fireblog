9648a3076262917f82e9674ea308fcea61a2d2c6
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-30 13:34:26 +0100

    swiper

---
+++ main.js
@@ -0,0 +1,6 @@
+const search = document.querySelector(".uil-search");
+const testInput = document.querySelector("input");
+
+search.addEventListener("click", () => {
+  testInput.style.display = "inline-block";
+});
